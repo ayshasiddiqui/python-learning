b4ff8faf7356147479700ec5c8400a005d096610
Asma Siddiqui <siddiayes@student.iul.ac.in>
2026-07-16 11:55:50 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+first_num= int(input("Enter your First Number: "))
+second_num= int(input("Enter your Second Number: "))
+difference= first_num-second_num
+print("difference of First Num and Second num is: " ,difference )
+first_num= int(input("Enter your First Number: "))
+second_num= int(input("Enter your Second Number: "))
+sum= first_num+second_num
+print("sum of First Num and Second num is: " ,sum)
+first_num= int(input("Enter your First Number: "))
+second_num= int(input("Enter your Second Number: "))
+multiplication= first_num*second_num
+print("multiplication of First Num and Second num is: " ,multiplication)
+first_num= int(input("Enter your First Number: "))
+second_num= int(input("Enter your Second Number: "))
+division= first_num/second_num
+print("division of First Num and Second num is: " ,division)
